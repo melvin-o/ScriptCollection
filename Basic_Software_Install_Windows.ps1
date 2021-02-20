ef2474c8e01e5998e8a5c0b6c224401ee3aae7ce
melvin-o <59124431+melvin-o@users.noreply.github.com>
2021-02-20 22:46:51 +0100

    Updated to use Windows Package Manager

--- Basic_Software_Install_Windows.ps1
+++ Basic_Software_Install_Windows.ps1
@@ -2,45 +2,15 @@
 <#Variables#>
 ###################################################################################################
 
-$DownloadFolder = "$env:SystemDrive\FOG_Downloads"
-$MSIexecLocation = [System.Environment]::SystemDirectory + "\msiexec.exe"
+$DownloadFolder = "$env:SystemDrive\Temp_Downloads"
 
 #Set variables for download links and local file location
-$ChromeDownloadLink = "https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi"
-$ChromeFileLocation = "$DownloadFolder\Google_Chrome.msi"
-
-$FirefoxDownloadLink = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=de"
-$FirefoxFileLocation = "$DownloadFolder\Mozilla_Firefox.msi"
-
-$7ZIPDownloadLink = "https://7-zip.org/a/7z1900-x64.msi"
-$7ZIPFileLocation = "$DownloadFolder\7z1900-x64.msi"
-
-$SteamDownloadLink = "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe"
-$SteamFileLocation = "$DownloadFolder\SteamSetup.exe"
-
 $TutanotaDownloadLink = "https://mail.tutanota.com/desktop/tutanota-desktop-win.exe"
 $TutanotaFileLocation = "$DownloadFolder\tutanota-desktop-win.exe"
 
-$SignalDownloadLink = "https://updates.signal.org/desktop/signal-desktop-win-1.40.0.exe"
-$SignalFileLocation = "$DownloadFolder\signal-desktop-win-1.40.0.exe"
-
-$KeepassXCDownloadLink = "https://github.com/keepassxreboot/keepassxc/releases/download/2.6.4/KeePassXC-2.6.4-Win64.msi"
-$KeepassXCFileLocation = "$DownloadFolder\KeePassXC-2.6.4-Win64.msi"
-
-$NotepadPlusPlusDownloadLink = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.3/npp.7.9.3.Installer.exe"
-$NotepadPlusPlusFileLocation = "$DownloadFolder\npp.7.9.3.Installer.exe"
-
-$OBSStudioDownloadLink = "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-26.1.1-Full-Installer-x64.exe"
-$OBSStudioFileLocation = "$DownloadFolder\OBS-Studio-26.1.1-Full-Installer-x64.exe"
-
-$OracleVirtualboxDownloadLink = "https://download.virtualbox.org/virtualbox/6.1.18/VirtualBox-6.1.18-142142-Win.exe"
-$OracleVirtualboxFileLocation = "$DownloadFolder\VirtualBox-6.1.18-142142-Win.exe"
-
-$SumatraPDFDownloadLink = "https://www.sumatrapdfreader.org/dl2/SumatraPDF-3.2-64-install.exe"
-$SumatraPDFFileLocation = "$DownloadFolder\SumatraPDF-3.2-64-install.exe"
-
-$VLCDownloadLink = "https://download.videolan.org/pub/videolan/vlc/3.0.12/win64/vlc-3.0.12-win64.msi"
-$VLCFileLocation = "$DownloadFolder\vlc-3.0.12-win64.msi"
+#Set variables for the current version of Windows Package Manager (Preview) | Needs to be updated when new version is released!
+$PackageManagerAppxBundleUri = "https://github.com/microsoft/winget-cli/releases/download/v-0.2.10191-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle"
+$PackageManagerAppxBundleFileLocation = "$DownloadFolder\PackageManager.appxbundle"
 
 ###################################################################################################
 <#Preperations#>
@@ -52,58 +22,38 @@ New-Item -Path $DownloadFolder -ItemType Directory -Force
 #Exclude the temporary folder from Windows Defender RTP and scheduled scans
 Add-MpPreference -ExclusionPath $DownloadFolder
 
+#Download and install Windows Package Manager
+Invoke-WebRequest -Uri $PackageManagerAppxBundleUri -OutFile $PackageManagerAppxBundleFileLocation
+Add-AppxPackage -Path $PackageManagerAppxBundleFileLocation
+
 ###################################################################################################
 <#Software Install#>
 ###################################################################################################
-
-#Download and install Google Chrome
-Invoke-WebRequest -Uri $ChromeDownloadLink -OutFile $ChromeFileLocation
-Start-Process -FilePath $MSIexecLocation -ArgumentList @("/i", "$ChromeFileLocation", "/quiet", "/norestart") -Wait
-
-#Download and install Mozilla Firefox
-Invoke-WebRequest -Uri $FirefoxDownloadLink -OutFile $FirefoxFileLocation
-Start-Process -FilePath $MSIexecLocation -ArgumentList @("/i", "$FirefoxFileLocation", "/quiet", "/norestart") -Wait
-
-#Download and install 7-ZIP 19.00
-Invoke-WebRequest -Uri $7ZIPDownloadLink -OutFile $7ZIPFileLocation
-Start-Process -FilePath $MSIexecLocation -ArgumentList @("/i", "$7ZIPFileLocation", "/quiet", "/norestart") -Wait
-
-#Download and install Steam
-Invoke-WebRequest -Uri $SteamDownloadLink -OutFile $SteamFileLocation
-Start-Process -FilePath $SteamFileLocation -ArgumentList @("/S") -Wait
 
 #Download and install Tutanota Desktop Client
 Invoke-WebRequest -Uri $TutanotaDownloadLink -OutFile $TutanotaFileLocation
 Start-Process -FilePath $TutanotaFileLocation -ArgumentList @("/allusers", "/S") -Wait
 
-#Download and install Signal Desktop 1.40
-Invoke-WebRequest -Uri $SignalDownloadLink -OutFile $SignalFileLocation
-Start-Process -FilePath $SignalFileLocation -ArgumentList @("/S") -Wait
-
-#Download and install KeepassXC 2.6.4
-Invoke-WebRequest -Uri $KeepassXCDownloadLink -OutFile $KeepassXCFileLocation
-Start-Process -FilePath $MSIexecLocation -ArgumentList @("/i", "$KeepassXCFileLocation", "/quiet", "/norestart") -Wait
-
-#Download and install Notepad++ 7.9.3
-Invoke-WebRequest -Uri $NotepadPlusPlusDownloadLink -OutFile $NotepadPlusPlusFileLocation
-Start-Process -FilePath $NotepadPlusPlusFileLocation -ArgumentList @("/S") -Wait
-
-#Download and install OBS-Studio 26.1.1
-Invoke-WebRequest -Uri $OBSStudioDownloadLink -OutFile $OBSStudioFileLocation
-Start-Process -FilePath $OBSStudioFileLocation -ArgumentList @("/S") -Wait
-
-#Download and install Oracle VM Virtualbox 6.1.18
-Invoke-WebRequest -Uri $OracleVirtualboxDownloadLink -OutFile $OracleVirtualboxFileLocation
-Start-Process -FilePath $OracleVirtualboxFileLocation -ArgumentList @("--silent", "--ignore-reboot") -Wait
-
-#Download and install SumatraPDF 3.2
-Invoke-WebRequest -Uri $SumatraPDFDownloadLink -OutFile $SumatraPDFFileLocation
-Start-Process -FilePath $SumatraPDFFileLocation -ArgumentList @("-s", "-with-filter", "-with-preview") -Wait
-
-#Download and install VLC 3.0.12
-Invoke-WebRequest -Uri $VLCDownloadLink -OutFile $VLCFileLocation
-Start-Process -FilePath $MSIexecLocation -ArgumentList @("/i", "$VLCFileLocation", "/quiet", "/norestart") -Wait
-
+winget install --id 7zip.7zip --silent
+winget install --id Mozilla.Firefox --silent
+winget install --id Google.Chrome --silent
+winget install --id KeePassXCTeam.KeePassXC --silent
+winget install --id Notepad++.Notepad++ --silent
+winget install --id OBSProject.OBSStudio --silent
+winget install --id Oracle.VirtualBox --silent
+winget install --id Signal.Signal --silent
+winget install --id Valve.Steam --silent
+winget install --id SumatraPDF.SumatraPDF --override "-install -s -with-filter -with-preview"
+winget install --id TeamSpeakSystems.TeamSpeakClient --silent
+winget install --id VideoLAN.VLC --silent
+winget install --id LibreOffice.LibreOffice --silent
+winget install --id Axosoft.GitKraken --silent
+winget install --id Rufus.Rufus --silent
+winget install --id BrutalChess.BrutalChess --silent
+winget install --id Nadeo.TrackManiaNationsForever --silent
+winget install --id WireGuard.WireGuard --silent
+winget install --id Microsoft.WindowsTerminal --silent
+winget install --id GitHub.GitHubDesktop --silent
 
 ###################################################################################################
 <#Cleanup#>
@@ -115,20 +65,5 @@ Remove-Item -Path $DownloadFolder -Recurse -Force
 #Remove Windows Defender exclusion
 Remove-MpPreference -ExclusionPath $DownloadFolder
 
-###################################################################################################
-<#Testing#>
-###################################################################################################
-
-<#
-$DISMLocation = [System.Environment]::SystemDirectory + "\dism.exe"
-$LinuxKernelUpdateDownloadLink = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
-$LinuxKernelUpdateFileLocation = "$DownloadFolder\wsl_update_x64.msi"
-
-#Install requirements for WSL2 (still needed in 20H2)
-Start-Process -FilePath $DISMLocation -ArgumentList @("/online", "/enable-feature", "/featurename:Microsoft-Windows-Subsystem-Linux", "/all", "/norestart")
-Start-Process -FilePath $DISMLocation -ArgumentList @("/online", "/enable-feature", "/featurename:VirtualMachinePlatform", "/all", "/norestart")
-#Restart-Computer, but have to continue script afterwards
-Invoke-WebRequest -Uri $LinuxKernelUpdateDownloadLink -OutFile $LinuxKernelUpdateFileLocation
-Start-Process -FilePath $MSIexecLocation -ArgumentList @("/i", "$LinuxKernelUpdateFileLocation", "/quiet", "/norestart") -Wait
-
-#>
+#Remove Windows Package Manager
+Get-AppXPackage Microsoft.DesktopAppInstaller | Remove-AppXPackage
